a3b8b2f12ae7424790a7c9b4e1c6f7b7d8d7888a
Neil Kane <neilkane1317@gmail.com>
2023-02-22 18:29:40 -0800

    added msg to parse valid string inputs and also msg if moving king into check

--- Chess/game_structure.py
+++ Chess/game_structure.py
@@ -10,14 +10,26 @@ def main():
                 print("You are in check!")
             print(board.legal_moves)
             start_square = input('Starting square:')
+            try:
+                start = chess.parse_square(start_square)
+            except:
+                print('Not a valid square')
+                continue
             end_square = input('Ending square:')
             try:
-                move = board.find_move(chess.parse_square(start_square), chess.parse_square(end_square))
+                print('Not a valid square')
+                end = chess.parse_square(end_square)
+            except:
+                continue
+            try:
+                move = board.find_move(start, end)
                 if move in board.legal_moves:
                     break
             except:
-                if board.is_pinned(board.turn,chess.parse_square(start_square)):
+                if board.is_pinned(board.turn,start):
                     print('Square ' + start_square + ' is pinned to your king!')
+                elif board.king(board.turn) == start and board.is_attacked_by(not board.turn, end):
+                    print('Cannot move your king into check!')
                 else:
                     print('Invalid move try again')
 
